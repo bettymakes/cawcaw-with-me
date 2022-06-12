7823e10b74003171f808593adfa2163028fe6290
bettymakes <makewithbetty@gmail.com>
2022-06-12 00:56:58 -0400

    Hook up basic interactivity
    - Add click events to all buttons
    - Add a counter to UI which increments on button clicks

--- README.md
+++ README.md
@@ -4,16 +4,17 @@
 ## 🎯 MVP goals
 
 ### 🧱 Basic functionality
-- [ ] Create a button for `woop woop`
+- [x] Create a button for `woop woop`
     - [ ] Clicking button plays a `woop woop` sound
-- [ ] Create a button for `caw caw`
+- [x] Create a button for `caw caw`
     - [ ] Clicking button plays a `caw caw` sound
-- [ ] Create a button for `wi-fives`
+- [x] Create a button for `wi-fives`
     - [ ] Clicking button plays a `wi-fives` sound
-- [ ] Create a button for `measure`
+- [x] Create a button for `measure`
     - [ ] Clicking button plays a `measure` sound
 - [ ] Add a counter for number of buttons clicked
     - [ ] Clear counter after `x` seconds
+
 
 ### 😎 UI upgrades
 - [ ] Add a nav bar
---
+++ js/main.js
@@ -0,0 +1,49 @@
+window.addEventListener('DOMContentLoaded', () => {  
+  $containerAllBtns = document.querySelector('#container-all-btns');
+
+  $btnCawCaw = document.querySelector('#cawcaw');
+  $btnWoopWoop = document.querySelector('#woopwoop');
+  $btnWiFive = document.querySelector('#wifive');
+  $btnMeasure = document.querySelector('#measure');
+
+  $counterCawCaw = document.querySelector('#counter-cawcaw');
+  $counterWoopWoop = document.querySelector('#counter-woopwoop');
+  $counterWiFive = document.querySelector('#counter-wifive');
+  $counterMeasure = document.querySelector('#counter-measure');
+
+  let cawcawCount = 0;
+  let woopwoopCount = 0;
+  let wifiveCount = 0;
+  let measureCount = 0;
+
+  $btnCawCaw.addEventListener('click', (event) => {
+    cawcawCount++;
+    $counterCawCaw.innerText = cawcawCount;
+    console.log(event);
+  });
+
+  $btnWoopWoop.addEventListener('click', (event) => {
+    woopwoopCount++;
+    $counterWoopWoop.innerText = woopwoopCount;
+    console.log(event);
+  });
+
+  $btnWiFive.addEventListener('click', (event) => {
+    wifiveCount++;
+    $counterWiFive.innerText = wifiveCount;
+    console.log(event);
+  });
+
+  $btnMeasure.addEventListener('click', (event) => {
+    measureCount++;
+    $counterMeasure.innerText = measureCount;
+    console.log(event);
+  });
+
+  // Events on button clicks will bubble up parents
+  // Capturing click event on parent which will use to reset counter
+  $containerAllBtns.addEventListener('click', (event) => {
+    console.log('target event', event.target)
+  });
+
+});
